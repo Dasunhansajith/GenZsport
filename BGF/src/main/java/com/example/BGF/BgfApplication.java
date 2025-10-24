28750241652cd74a1087cf63be5ba214ac70a7eb
Dasun Hansajith Ariyasena <dasunhansajith@gmail.com>
2025-10-24 20:19:04 +0530

    add new files

--- BGF/src/main/java/com/example/BGF/BgfApplication.java
+++ BGF/src/main/java/com/example/BGF/BgfApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BgfApplication {
+public class     BgfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BgfApplication.class, args);
 	}
 
 }
+
+
+
+
 
 
 
